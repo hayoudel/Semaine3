4cae9fd8d58c49fa988be9da82c3eba33af40702
Delphine Hayicou <hayoudel@gmail.com>
2025-07-11 12:17:33 +0000

    exo

---
+++ operation.php
@@ -0,0 +1,26 @@
+
+<?php 
+$var_nom = 'Hayicou Delphine' ;
+$var_age =  14;
+$var_statut = 'Célibataire';
+$var_formation  = 'Informatique';
+echo 'Je suis Mademoiselle '. $var_nom . ' j \'ai ' . $var_age . ' je suis '. $var_statut . ' et je travaille dans le domaine de l\' ' . $var_formation . '<br>';
+
+
+$a = 15;
+$b = 4;  
+$addition = $a + $b;
+$soustraction = $a -$b;
+$multiplication = $a * $b;
+$division = $a / $b;
+$modulo = $a % $b;
+echo 'a = '. $a . ' b = '. $b . '<br>' ;
+echo 'l\'addition de a + b = ' . $addition . '<br>';
+echo 'La soustraction de a - b = '. $soustraction . '<br>';
+echo 'La multiplication de a * b = '. $multiplication . '<br>';
+echo 'La division de a par b  = '. $division . '<br>' ;
+echo 'le modulo de a sur b = ' . $modulo ;
+
+
+
+?>
